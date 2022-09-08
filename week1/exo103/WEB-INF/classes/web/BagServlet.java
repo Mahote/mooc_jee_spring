a5711df625e08121dacaa5e32a00fdc4d723eed5
Thomas Brunet <thomas.brunet.etu@univ-lille.fr>
2022-09-08 13:46:22 +0200

    réalisation exo 103 param et property

--- week1/exo103/WEB-INF/classes/web/BagServlet.java
+++ week1/exo103/WEB-INF/classes/web/BagServlet.java
@@ -6,29 +6,53 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
 
 // TODO: this class should extends something to be a usable servlet.
 // TODO: add an annotation here to map your servlet on an URL.
-public class BagServlet {
+@WebServlet("/bag")
+public class BagServlet extends HttpServlet {
 	
 	Bag myBag = new Bag();
-	
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
+
+		out.write("<body>");
+		out.write("<form method=\"post\">");
+		out.write("ref : <input name=\"ref\"> <BR>");
+		out.write("qty : <input name=\"qty\"> <BR>");
+		out.write("<input type=\"submit\" value=\"envoi\"></form>");
+		out.write("</body>");
 		
 		// TODO : print a html form using printwriter.
 	}
 
 	
-	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-		
-		// TODO : Get parameters, check null
-		
-		// TODO : print reference and quantity
+		String ref = req.getParameter("ref");
+		String qty = req.getParameter("qty");
+		int qtyint = 0;
+		if(ref == null || ref.isEmpty()){
+			res.sendError(400);
+		}
+		try{
+				qtyint = Integer.valueOf(qty);
+			}
+		catch(NumberFormatException e){
+				res.sendError(400);
+			}
+		if(qty == null || qty.isEmpty()){
+			res.sendError(400);
+		}
+		myBag.setItem(ref, qtyint);
+		res.sendRedirect("/bag");
 
 	}
 	
